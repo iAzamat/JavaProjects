35bb24839f19e3d847ce7d2e128ebffb4fd04b42
Azamat <isabirovazamati@gmail.com>
2022-07-09 14:29:34 +0500

    Reformat Code

--- Introduction001_MaxSearch/src/Main.java
+++ Introduction001_MaxSearch/src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int a=1;
-        int b=8;
-        int c=3;
-        int d=2;
-        int e=6;
-        int max=a;
-        if(b>max) max=b;
-        if(c>max) max=c;
-        if(d>max) max=d;
-        if(e>max) max=e;
-        System.out.println("Самая тяжелая гиря "+max);
+        int a = 1;
+        int b = 8;
+        int c = 3;
+        int d = 2;
+        int e = 6;
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        if (d > max) max = d;
+        if (e > max) max = e;
+        System.out.println("Самая тяжелая гиря " + max);
     }
 }
