ab341a4afe9561809767aedcae146f62d6f9992a
Azamat <isabirovazamati@gmail.com>
2022-07-16 13:56:50 +0500

    Добавлена 90 задача

--- Course0/src/ex090_test.java
+++ Course0/src/ex090_test.java
@@ -10,5 +10,20 @@ Sample Output:
  */
 
 import java.util.Scanner;
+
 public class ex090_test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long number = Math.abs(sc.nextInt());
+        int maxDigit;
+        maxDigit = (int) (number % 10);
+        while (number > 0) {
+            int curDigit = (int) (number % 10);
+            if (curDigit > maxDigit)
+                maxDigit = curDigit;
+            number /= 10;
+        }
+
+        System.out.println(maxDigit);
+    }
 }
